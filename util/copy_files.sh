be104a6c906242f75457baca134bf3cfbf4e8b76
james abel <j@abel.co>
2016-10-23 15:04:30 -0700

    don't copy the make_venv.sh

--- util/copy_files.sh
+++ util/copy_files.sh
@@ -5,5 +5,4 @@ set -x
 if [ -z "$1" ]; then echo "error: parameter 1 not set - exiting"; exit; fi
 if [ -z "$2" ]; then echo "error: parameter 2 not set - exiting"; exit; fi
 cp -f $1/main.py $2
-cp -f $1/make_venv.sh $2
 cp -f $1/main.sh $2
