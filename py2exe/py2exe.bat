1ab5dcc279ccd5d9757da753d52b69fea62f8919
james abel <j@abel.co>
2016-10-23 11:29:52 -0700

    add copy files

--- py2exe/py2exe.bat
+++ py2exe/py2exe.bat
@@ -1,4 +1,5 @@
 rmdir /s /q dist
+..\util\copy_files.bat .. .
 REM we would like to use a venv but PyQt5 on Python 3.4 does not install to a venv
 REM and we are on Python 3.4 since py2exe does not support Python 3.5
 c:\python34\Scripts\pip3.exe install -U pip
