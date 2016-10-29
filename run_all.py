c06b17132d24214f32eb802077bd6e78f57703d4
james abel <j@abel.co>
2016-10-29 12:19:57 -0700

    add placeholder for briefcase win

--- run_all.py
+++ run_all.py
@@ -12,6 +12,7 @@ def main():
         subprocess.check_call('./test_py2app.sh', cwd='test_py2app', shell=True)
         subprocess.check_call('./test_pyinstaller_mac.sh', cwd='test_pyinstaller_mac', shell=True)
     elif spafit.is_windows():
+        subprocess.check_call('test_briefcase_win.bat', cwd='test_briefcase_win', shell=True)
         subprocess.check_call('test_cx_freeze_win.bat', cwd='test_cx_freeze_win', shell=True)
         subprocess.check_call('test_osnap_win.bat', cwd='test_osnap_win', shell=True)
         subprocess.check_call('test_py2exe.bat', cwd='test_py2exe', shell=True)
