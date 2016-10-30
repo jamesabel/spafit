e7cf5b1fee814c5bad72ea2a691850f74b8d2dd6
james abel <j@abel.co>
2016-10-30 15:05:11 -0700

    create report

--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
-
 ## SPAFIT - Small Python Application Freezer/Installer Test ##
-
 Notes
-- Generally tried to use Python 3.5.  If a tool failed with 3.5 I tried 3.4.
-  However, no tool that failed on 3.5 then passed on 3.4.
-
+- Generally tried to use Python 3.5.  If a tool failed with 3.5 I tried 3.4.  However, no tool that failed on 3.5 then passed on 3.4.
+    
+| Tool | Win | Mac | Issue |
+|------|-----|-----|-------|
+| briefcase | :no_entry: Not Supported | :x: FAIL | [] |
+| cx_freeze | :x: FAIL | :x: FAIL | [] |
+| osnap | :white_check_mark: PASS | :white_check_mark: PASS | [] |
+| py2app | :no_entry: Not Supported | :x: FAIL | [] |
+| py2exe | :x: FAILED | :no_entry: Not Supported | [] |
+| pyinstaller | :x: FAIL | :x: FAIL | [] |
+| pynsist | :white_check_mark: PASS | :no_entry: Not Supported | [] |
--- spafit/rollup.py
+++ spafit/rollup.py
@@ -1,6 +1,61 @@
 
 import glob
+import re
+import os
+import collections
+
+
+def test_name_os_from_dir(test_dir):
+    """
+    takes a test name and returns a tuple with the test name and the OS
+    :param test_dir: raw name of test dir
+    :return: tuple with <test_name> , <OS>
+    """
+    # tolerates _ in test name
+    s = re.search(r'test_([0-9a-z_]+)_(mac|win)', test_dir)
+    return s.group(1), s.group(2)
+
+
+def test_os_to_strings(test_name, supported, test_os, issues):
+    if supported:
+        status_file_path = os.path.join('test_' + test_name + '_' + test_os, test_name.replace('test_', '') + '_' + test_os + '_status.log')
+        if os.path.exists(status_file_path):
+            with open(status_file_path) as f:
+                status = f.readline().strip()
+            if 'pass' in status.lower():
+                status = ':white_check_mark: %s' % status
+            else:
+                status = ':x: %s' % status
+        else:
+            status = ':x: FAILED'
+        # issues.append(test_name)
+    else:
+        status = ':no_entry: Not Supported'
+    return status
+
 
 def rollup():
-    for d in glob.glob('test_*'):
-        print(d)
+    tests = collections.defaultdict(list)
+    for d in [d for d in glob.glob('test_*')]:
+        test, test_os = test_name_os_from_dir(d)
+        tests[test].append(test_os)
+    print(tests)
+
+    intro = "\
+## SPAFIT - Small Python Application Freezer/Installer Test ##\n\
+\
+Notes\n\
+- Generally tried to use Python 3.5.  If a tool failed with 3.5 I tried 3.4.\
+  However, no tool that failed on 3.5 then passed on 3.4.\n\
+    "
+
+    with open('readme.md', 'w') as f:
+        f.write(intro)
+        f.write('\n')
+        f.write('| Tool | Win | Mac | Issue |\n')
+        f.write('|------|-----|-----|-------|\n')
+        for test in sorted(tests):
+            issues = []
+            win = test_os_to_strings(test, 'win' in tests[test], 'win', issues)
+            mac = test_os_to_strings(test, 'mac' in tests[test], 'mac', issues)
+            f.write('| %s | %s | %s | %s |\n' % (test, win, mac, str(issues)))
