b6fba5ff239209cb9be2f3f8d588452f700b10ca
james abel <j@abel.co>
2016-10-30 12:00:56 -0700

    put os in every test name

--- run_all.py
+++ run_all.py
@@ -9,16 +9,16 @@ def main():
         subprocess.check_call('./test_briefcase_mac.sh', cwd='test_briefcase_mac', shell=True)
         subprocess.check_call('./test_cx_freeze_mac.sh', cwd='test_cx_freeze_mac', shell=True)
         subprocess.check_call('./test_osnap_mac.sh', cwd='test_osnap_mac', shell=True)
-        subprocess.check_call('./test_py2app.sh', cwd='test_py2app', shell=True)
+        subprocess.check_call('./test_py2app_mac.sh', cwd='test_py2app_mac', shell=True)
         subprocess.check_call('./test_pyinstaller_mac.sh', cwd='test_pyinstaller_mac', shell=True)
     elif spafit.is_windows():
         # briefcase does not yet support Windows
         # subprocess.check_call('test_briefcase_win.bat', cwd='test_briefcase_win', shell=True)
         subprocess.check_call('test_cx_freeze_win.bat', cwd='test_cx_freeze_win', shell=True)
         subprocess.check_call('test_osnap_win.bat', cwd='test_osnap_win', shell=True)
-        subprocess.check_call('test_py2exe.bat', cwd='test_py2exe', shell=True)
+        subprocess.check_call('test_py2exe_win.bat', cwd='test_py2exe_win', shell=True)
         subprocess.check_call('test_pyinstaller_win.bat', cwd='test_pyinstaller_win', shell=True)
-        subprocess.check_call('test_pynsist.bat', cwd='test_pynsist', shell=True)
+        subprocess.check_call('test_pynsist_win.bat', cwd='test_pynsist_win', shell=True)
 
 if __name__ == '__main__':
     main()
